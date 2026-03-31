f522343c39bfc43c5c78d0578c2ed8aeca2e168e
KarenStephy <k.musangwa@alustudent.com>
2026-03-31 09:20:00 -0700

    python more data

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    max = list(a_dictionary.items())[0][1]
+    for key, value in a_dictionary.items():
+        if value > max:
+            max = value
+    for key, value in a_dictionary.items():
+        if value == max:
+            return key
