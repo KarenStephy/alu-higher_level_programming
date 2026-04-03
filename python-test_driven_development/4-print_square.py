aff66641374b1b15ee2781f0a9272d7ad2ce4a42
KarenStephy <k.musangwa@alustudent.com>
2026-04-04 01:14:46 +0200

    Improve docstring for print_square function
    Update docstring to clarify function purpose.

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,10 +1,12 @@
-
 #!/usr/bin/python3
-"""print square function"""
+"""
+This is the "4-print_square" module.
+The 4-print_square  module supplies one function, print_square(size).
+"""
 
 
 def print_square(size):
-    """prints a square with "#" size."""
+    """prints a square with "#"'s that has a length of size """
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
